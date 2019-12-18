dab6b2b7ae3829a0615690a85dcff0966f5881b5
Nicolas <suporte@morinfo.com.br>
2019-12-18 20:47:51 -0300

    acerto

--- public/js/cadastroUsuario.js
+++ public/js/cadastroUsuario.js
@@ -167,5 +167,58 @@ function prevenir(){
   event.preventDefault()
 }
 
+function atualizaTabela(){
+  $ ('#teste').DataTable().ajax.reload();
+}
+function mostrarTabela(){
+    $(document).ready(function() {
+        $('#teste').DataTable( {           
+            "ajax": "../users",
+            "columns": [
+                { "data": "username" },
+                { "data": "name" },
+                { "data": "email" },
+                { "data": "admin" }
+            ],
+            "language": idioma,
+            buttons: [
+              'copy', 'excel', 'pdf'
+          ]
+        })
 
+    })
+    var idioma = {
+      
+    "sEmptyTable": "Nenhum registro encontrado",
+    "sInfo": "Mostrando de _START_ até _END_ de _TOTAL_ registros",
+    "sInfoEmpty": "Mostrando 0 até 0 de 0 registros",
+    "sInfoFiltered": "(Filtrados de _MAX_ registros)",
+    "sInfoPostFix": "",
+    "sInfoThousands": ".",
+    "sLengthMenu": "_MENU_ resultados por página",
+    "sLoadingRecords": "Carregando...",
+    "sProcessing": "Processando...",
+    "sZeroRecords": "Nenhum registro encontrado",
+    "sSearch": "Pesquisar",
+    "oPaginate": {
+        "sNext": "Próximo",
+        "sPrevious": "Anterior",
+        "sFirst": "Primeiro",
+        "sLast": "Último"
+    },
+    "oAria": {
+        "sSortAscending": ": Ordenar colunas de forma ascendente",
+        "sSortDescending": ": Ordenar colunas de forma descendente"
+    },
+    "select": {
+        "rows": {
+            "_": "Selecionado %d linhas",
+            "0": "Nenhuma linha selecionada",
+            "1": "Selecionado 1 linha"
+        }
+    }
+}  
+}
+
+mostrarTabela()
 
