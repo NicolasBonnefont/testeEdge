23ab1624be8c7ec4b1cc6d56929d29d62875f7aa
Nicolas <suporte@morinfo.com.br>
2020-01-09 14:18:04 -0300

    acerto

--- public/js/perfil.js
+++ public/js/perfil.js
@@ -1,5 +1,9 @@
+function novaImg(){
 var data = sessionStorage.getItem("user")
 const u = JSON.parse(data)
 document.getElementById("imgPerfil").src = u.url
 document.getElementById("nomePerfil").innerHTML = u.name
 $('#exampleModal').modal(show)
+}
+
+novaImg()
