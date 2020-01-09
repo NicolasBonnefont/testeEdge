3b601c1c5acb129d680e7674bc34b3a0b6752561
Nicolas <suporte@morinfo.com.br>
2020-01-09 16:00:26 -0300

    numsey

--- public/js/perfil.js
+++ public/js/perfil.js
@@ -4,7 +4,7 @@ var data = sessionStorage.getItem("user")
 const u = JSON.parse(data)
 document.getElementById("imgPerfil").src = u.url
 document.getElementById("nomePerfil").innerHTML = u.name
-document.getElementById("nomePerfil").innerHTML = u.cargo
+document.getElementById("cargoPerfil").innerHTML = u.cargo
 $('#exampleModal').modal(show)
 
 }
