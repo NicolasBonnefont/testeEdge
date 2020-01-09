5c27612b233bce6c27326f6e3e587e8d4bc01a44
Nicolas <suporte@morinfo.com.br>
2020-01-09 10:47:41 -0300

    acerto

--- public/js/perfil.js
+++ public/js/perfil.js
@@ -1,7 +1,4 @@
-function imgPerfil(){
- 
-  var data = sessionStorage.getItem("user")
-  const u = JSON.parse(data)  
-  document.getElementById("imgPerfil").src = u.url
-  
-}
+var data = sessionStorage.getItem("user")
+const u = JSON.parse(data)
+document.getElementById("imgPerfil").src = u.url
+console.log("url = " + u.url)
