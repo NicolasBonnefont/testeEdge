2b787c058a69d972907ef413e5b6837242edd45c
Nicolas <suporte@morinfo.com.br>
2020-01-09 13:51:40 -0300

    acerto

--- public/js/perfil.js
+++ public/js/perfil.js
@@ -2,3 +2,4 @@ var data = sessionStorage.getItem("user")
 const u = JSON.parse(data)
 document.getElementById("imgPerfil").src = u.url
 document.getElementById("nomePerfil").innerHTML = u.name
+$('#exampleModal').modal(show)
