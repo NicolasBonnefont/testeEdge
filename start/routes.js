b44b06fa931402f2d9ece4a770252bc99c8f36ba
Nicolas <suporte@morinfo.com.br>
2020-01-14 20:03:48 -0300

    acerto

--- start/routes.js
+++ start/routes.js
@@ -45,7 +45,7 @@ Route.get('notificacao',"NotificacaoController.index")
 Route.put('notificacao',"NotificacaoController.update")
 
 Route.put('ip',"ConfigIpController.update")
-Route.get('ip',"ConfigIpController.show")
+Route.get('ip',"ConfigIpController.index")
 Route.post('ip',"ConfigIpController.store")
 
 
